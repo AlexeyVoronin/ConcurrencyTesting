a5a57d2c1aa856f3745ca0ac37fa9ff8f85cb2e9
Alexey Voronin <170282@mail.ru>
2012-10-03 22:49:23 +0400

    Old files removed.

---
+++ libs/NMock3/Tutorial/NMock3.Tutorial/FAQ/PropertyExamples.cs
@@ -0,0 +1,49 @@
+﻿#region Using
+#if NUNIT
+using NMock.Matchers;
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestContext = System.Object;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+#endif
+
+using System;
+using NMock;
+#endregion
+
+namespace NMock3.Tutorial.FAQ
+{
+	[TestClass]
+	public class PropertyExamples : ExamplesBase
+	{
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void HowToThrowAnExceptionOnAPropertySetter()
+		{
+			Mock<IContactManagementView> view = Factory.CreateMock<IContactManagementView>();
+
+			view.Expects.One.SetProperty(v => v.Name).ToAnything().Will(Throw.Exception(new InvalidOperationException()));
+
+			view.MockObject.Name = new Name();
+		}
+
+		[Test]
+		public void HowToUseAMatcherOnAPropertySetter()
+		{
+			Mock<IContactManagementView> view = Factory.CreateMock<IContactManagementView>();
+
+		  view.Expects.One.SetProperty(v => v.ContactType).To(new EqualMatcher(ContactType.Vendor));
+
+		  view.MockObject.ContactType = ContactType.None;
+
+      Factory.VerifyAllExpectationsHaveBeenMet();
+
+		  //view.MockObject.Name = new Name();
+		}
+	}
+}
---
+++ libs/NMock3/Tutorial/ReadMe.txt
@@ -0,0 +1,12 @@
+
+Tutorial Setup:
+
+1. The tutorials use NUnit or MSTest.  When you open the project in Visual Studio,
+ensure that the project builds.  You may have to fix some of your references.  If
+you prefer to use NUnit, look at the introduction in Lesson 0, Exercise 1.
+
+2. Try running the tests in Lesson 0, Exercise 1 right away.  Windows may have
+blocked the "dll" files from the zip file when it was downloaded.  You will know
+if this is the case if Visual Studio says that the dlls are untrusted.  To fix 
+this problem, from Windows Explorer, right-click on the dll and select properties.
+On the main tab, at the bottom, click on the "unblock" button for all dll files.
